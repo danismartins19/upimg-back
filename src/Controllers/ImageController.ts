09272f0d1496d976554dfad9eac845922f266703
Daniel Martins <danidiamoo1@gmail.com>
2022-08-15 21:10:09 -0300

    teste de rota de upload

--- src/Controllers/ImageController.ts
+++ src/Controllers/ImageController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 
 export const addImage = async (req: Request, res: Response) => {
-    console.log('')
+    let { img } = req.body;
+    console.log(img);
 }
 
 export const getImage = async (req: Request, res: Response) => {
